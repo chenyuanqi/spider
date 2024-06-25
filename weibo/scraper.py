799949df53a6feeb7366a5d103d95cf2f648b4c6
chenyuanqi <chenyuanqi@outlook.com>
2024-06-25 22:27:51 +0800

    feat: ✨ 添加微博爬虫的使用

---
+++ weibo/scraper.py
@@ -0,0 +1,9 @@
+
+# 使用 https://github.com/dataabc/weiboSpider
+# python3 -m pip install weibo-spider
+# 配置文件参考
+# https://github.com/dataabc/weiboSpider/blob/master/docs/settings.md
+
+
+# 使用方法
+# python3 -m weibo_spider --config_path="config.json"
